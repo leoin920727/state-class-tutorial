48a8abbdfbf8fa4021dc10a975e053e19f4c475d
Lin Jheng Bing <leoin920727@gmail.com>
2023-09-13 00:22:17 +0800

    Self-made HOOK, you can render when the data changes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,21 +4,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from '../Dashboard/Dashboard';
 import Login from '../Login/Login';
 import Preferences from '../Preferences/Preferences';
-
-function setToken(userToken) {//把後端傳遞的key:value放到從sessionStorage
-  sessionStorage.setItem('token', JSON.stringify(userToken));//參數放入
-}
-//sessionStorage.clear()清除
-function getToken() {//從sessionStorage抓取token
-  const tokenString = sessionStorage.getItem('token');
-  const userToken = JSON.parse(tokenString);
-  return userToken?.token//Optional_chaining 後端資料中的token
-  //沒打?會觸發Cannot read properties of null (reading 'token')
-}
+import useToken from './useToken';
 
 function App() {
-  // const [token, setToken] = useState();
-  const token = getToken();
+  const { token, setToken } = useToken();
   
   if(!token) {
     return <Login setToken={setToken} />
